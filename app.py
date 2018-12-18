8e23ddc2ab5e970d070496a48484c4740597b0e1
emschenn <emilynov14@gmail.com>
2018-12-19 02:24:48 +0800

    Test

--- app.py
+++ app.py
@@ -93,8 +93,8 @@ def webhook_handler():
 
 @route('/show-fsm', methods=['GET'])
 def show_fsm():
-    machine.get_graph().draw('fsm.png', prog='dot', format='png')
-    return static_file('fsm.png', root='./', mimetype='image/png')
+#    machine.get_graph().draw('fsm.png', prog='dot', format='png')
+ #   return static_file('fsm.png', root='./', mimetype='image/png')
 
 
 if __name__ == "__main__":
